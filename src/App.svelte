7ca4fabdbdda87e3ebe36e245b74cb35fb8730ca
seon-mikim <tjsalsun@gmail.com>
2023-08-11 10:27:52 +0900

    conf: 파이어베이스 배포

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,22 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+	import Login from "./pages/Login.svelte";
+	import Main from "./pages/Main.svelte";
+	import NotFound from "./pages/NotFound.svelte";
+	import Signup from "./pages/Signup.svelte";
+	import Write from "./pages/Write.svelte";
+  import Router from 'svelte-spa-router'
+  import './css/style.css'
+  
+  const routes = {
+    '/': Main,
+    '/login': Login,
+    '/singup': Signup,
+    '/write': Write,
+    '*':NotFound
+  }
+
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<Router {routes}/>
 
-  <div class="card">
-    <Counter />
-  </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
---
+++ src/pages/Main.svelte
@@ -0,0 +1,59 @@
+<script>
+	let hour = new Date().getHours()
+	let min = new Date().getMinutes()
+</script>
+
+<header>
+		<div class="info-bar">
+			<div class="info-bar__time">{hour}:{min}</div>
+			<div class="info-bar__icons">
+				<img src="assets/cart-bar.svg" alt="cart-bar">
+				<img src="assets/wifi.svg" alt="wifi">
+				<img src="assets/battery.svg" alt="battery">
+			</div>
+		</div>
+		<div class="menu-bar">
+			<div class="menu-bar__location">
+				<div>
+					역삼1동
+				</div>				
+				<img src="assets/arrow-down.svg" alt="arrow-down">
+			</div>
+			<div class="menu-bar__icons">
+				<img src="assets/search.svg" alt="search">
+				<img src="assets/bar.svg" alt="bar">
+				<img src="assets/bell.svg" alt="bell">
+			</div>
+		</div>
+	</header>
+	<main>
+		<a href="#/write" class="write-btn">+ 글쓰기</a>
+	</main>
+	<footer>
+		<div class="footer-block">
+			<div class="footer-icon">
+				<img src="assets/fill-home.svg" alt="home">
+				<div class="footer-text">홈</div>
+			</div>
+			<div class="footer-icon">
+				<img src="assets/newspaper.svg" alt="동네생활">
+				<div class="footer-text">동네생활</div>
+			</div>
+			<div class="footer-icon">
+				<img src="assets/pin.svg" alt="내 근처">
+				<div class="footer-text">내 근처</div>
+			</div>
+			<div class="footer-icon">
+				<img src="assets/chat.svg" alt="채팅">
+				<div class="footer-text">채팅</div>
+			</div>
+			<div class="footer-icon">
+				<img src="assets/user.svg" alt="나의 당근">
+				<div class="footer-text">나의 당근</div>
+			</div>
+		</div>
+	</footer>
+
+	<style>
+
+	</style>
